c14fb0a57eeb5b15c8c9b413f65ff07d8ddd60c5
hunny345686 <2118165@cognizant.com>
2023-08-10 16:02:22 +0530

    first commit

---
+++ netflixClone/src/components/navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import { ArrowDropDown, Notifications, Search } from "@mui/icons-material";
+import { useState } from "react";
+import "./navbar.scss";
+import logo from "../../assets/imgs/netflixLogo.png"
+import defaultUser from "../../assets/imgs/DefaultUserImg.png"
+
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="wrapper">
+        <div className="left">
+          <img
+            src={logo}
+            alt="logo"
+          />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src={defaultUser}
+            alt="User"
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
